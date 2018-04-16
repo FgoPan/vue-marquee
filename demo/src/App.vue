d9bc22498419b66032ae1bfa80780fb7893bc1f2
pansiyan <pansiyan@t2cloud.net>
2018-04-16 14:30:50 +0800

    很low的vue跑马灯

---
+++ demo/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="out-box">
+    <vueMarquee :data="msg" :style-object="styleObject"/>
+  </div>
+</template>
+<script>
+import vueMarquee from './components/vue-marquee'
+
+export default {
+  name: 'App',
+  components: {
+    vueMarquee
+  },
+  data() {
+    return {
+      msg: '谁在乎我的心里有多苦，谁在意我的明天去何处，这条路究竟多少崎岖多少坎坷途，我和你早以没有回头路',
+      styleObject: {
+        color: 'red',
+        fontSize: '13px'
+      }
+    }
+  }
+}
+
+</script>
+<style>
+.out-box {
+  width: 300px;
+  margin: 0 auto;
+  border: 1px solid #ff6700;
+}
+
+</style>
---
+++ demo/src/components/vue-marquee.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="box">
+    <p class="animate" :style="styleObject">
+      {{data}}
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'marquee',
+  props: {
+    data: {
+      required: true,
+      type: String,
+      default: ''
+    },
+    styleObject: {
+      type: Object,
+      default:{}
+    }
+  }
+}
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.box {
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+}
+.animate {
+  padding-left: 20px;
+  font-size: 12px;
+  color: #000;
+  display: inline-block;
+  white-space: nowrap;
+  animation: 15s Loop linear infinite normal;
+}
+
+@keyframes Loop {
+  0% {
+    transform: translateX(0px);
+    -webkit-transform: translateX(0px);
+  }
+  100% {
+    transform: translateX(-100%);
+    -webkit-transform: translateX(-100%);
+  }
+}
+
+@-webkit-keyframes Loop {
+  0% {
+    transform: translateX(0px);
+    -webkit-transform: translateX(0px);
+  }
+  100% {
+    transform: translateX(-100%);
+    -webkit-transform: translateX(-100%);
+  }
+}
+
+</style>
